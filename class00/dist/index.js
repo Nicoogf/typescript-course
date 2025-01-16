07738f09b0b299f91c2fa31cc188982c3bdcc84b
Nicolas <nicolasgfalabella@gmail.com>
2025-01-16 08:45:07 -0300

    Tipado

---
+++ class00/dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let myString = "Hola mundo";
+let myNumber = 123;
+let myBool = false;
+let myArrayNumber = [1, 2, 23];
+let myStringNumber = ["Hola mundo"];
+let myTuple = [12, "Juan"];
+let myTupleOfTuple;
+myTupleOfTuple = [
+    [1, "Fede"],
+    [2, "Juan"]
+];
+let nuevaVariable;
+console.log(typeof (nuevaVariable));
---
+++ class00/src/index.ts
@@ -0,0 +1,28 @@
+let myString : String = "Hola mundo";
+let myNumber : number = 123 ;
+let myBool : boolean = false ;
+let myArrayNumber : number[] = [1,2,23]
+let myStringNumber: string[] = ["Hola mundo"]
+
+let myTuple : [number,string] = [12,"Juan"]
+let myTupleOfTuple : [number,string] []
+
+myTupleOfTuple = [
+    [1,"Fede"],
+    [2,"Juan"]
+]
+
+let nuevaVariable : string ;
+
+nuevaVariable = "Nicolas"
+
+const isMatch = 1 
+
+const nombre = "Nicolas"
+
+
+//
+
+let myName: String | number | null 
+
+myName = 7
